5ea4dfbba24b4b10c57de8d5b4a4cbb0c845b574
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-12-26 22:33:26 +0530

    plain window with bg create

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
+
+# --------------------- UI SETUP ------------------------ #
+# Creating a window
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50)
+
+window.config(bg=BACKGROUND_COLOR)
+
+
+window.mainloop()
