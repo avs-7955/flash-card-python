0e6c14416ec23ab7b451ea6a1bec50f9513cdf64
Ayushi Vineet Sharan <ayushi.v.sharan@gmail.com>
2022-12-27 12:54:23 +0530

    Generate random card word when btn pressed

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from tkinter import *
+import pandas as pd
+import random
 BACKGROUND_COLOR = "#B1DDC6"
+
+# --------------------- FLASH CARD RANDOMIZATION ------------------------ #
+df = pd.read_csv('./data/word_meanings.csv')
+word_dict = df.to_dict('records')
+# print(word_dict[3]['WORD'])
+
+
+def random_card():
+    '''Generate random word for cards'''
+    rand_num = int(random.random() * len(word_dict))
+    rand_word = word_dict[rand_num]['WORD']
+    canvas.itemconfig(word, text=rand_word)
+
 
 # --------------------- UI SETUP ------------------------ #
 # Creating a window
@@ -23,11 +38,13 @@ word = canvas.create_text(
 
 # Creating the btns
 wrong = PhotoImage(file="./images/wrong.png")
-button_w = Button(image=wrong, highlightthickness=0, borderwidth=0)
+button_w = Button(image=wrong, highlightthickness=0,
+                  borderwidth=0, command=random_card)
 button_w.grid(row=1, column=0)
 
 correct = PhotoImage(file="./images/right.png")
-button_c = Button(image=correct, highlightthickness=0, borderwidth=0)
+button_c = Button(image=correct, highlightthickness=0,
+                  borderwidth=0, command=random_card)
 button_c.grid(row=1, column=1)
 
 window.mainloop()
